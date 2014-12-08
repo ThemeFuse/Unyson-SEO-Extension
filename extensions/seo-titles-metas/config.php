a130697b4aac8f4c34bcddd6943fa9ed9c93ed8e
themefuse <hello@themefuse.com>
2014-12-08 17:46:02 +0200

    added titles and meta config phpDoc

--- extensions/seo-titles-metas/config.php
+++ extensions/seo-titles-metas/config.php
@@ -4,5 +4,12 @@
 
 $cfg = array();
 
+/**
+ * Posts types that you want to exclude from titles and meta settings
+ */
 $cfg['excluded_post_types'] = array( 'attachment' );
+
+/**
+ * Taxonomies that you want to exclude from titles and meta settings.
+ */
 $cfg['excluded_taxonomies'] = array( 'post_tag' );
