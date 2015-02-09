ffa615ced75ee3723c9c4204872cb3e9eeedcccc
Pisarenco Gheorghe <pisarenco.gh@gmail.com>
2015-02-09 10:06:02 +0200

    New Version

--- manifest.php
+++ manifest.php
@@ -6,7 +6,7 @@ $manifest = array();
 
 $manifest['name']        = __( 'SEO', 'fw' );
 $manifest['description'] = __( 'This extension will enable you to have a fully optimized WordPress website by adding optimized meta titles, keywords and descriptions.', 'fw' );
-$manifest['version'] = '1.0.1';
+$manifest['version'] = '1.0.2';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
