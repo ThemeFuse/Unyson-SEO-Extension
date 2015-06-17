d7a741d19cb5b2bc64aa943a6adbe51cc9137832
moldcraft <moldcraft@email.com>
2015-06-17 16:08:12 +0300

    Version 1.0.3

--- manifest.php
+++ manifest.php
@@ -5,8 +5,12 @@
 $manifest = array();
 
 $manifest['name']        = __( 'SEO', 'fw' );
-$manifest['description'] = __( 'This extension will enable you to have a fully optimized WordPress website by adding optimized meta titles, keywords and descriptions.', 'fw' );
-$manifest['version'] = '1.0.2';
+$manifest['description'] = __(
+	'This extension will enable you to have a fully optimized WordPress website'
+	.' by adding optimized meta titles, keywords and descriptions.',
+	'fw'
+);
+$manifest['version'] = '1.0.3';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
