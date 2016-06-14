29b2c1c6e9bc0af1749f7e57676b97dbd638f81f
moldcraft <moldcraft@email.com>
2016-06-14 15:34:41 +0300

    Version 1.0.7

--- manifest.php
+++ manifest.php
@@ -10,7 +10,7 @@ $manifest['description'] = __(
 	.' by adding optimized meta titles, keywords and descriptions.',
 	'fw'
 );
-$manifest['version'] = '1.0.6';
+$manifest['version'] = '1.0.7';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
 
