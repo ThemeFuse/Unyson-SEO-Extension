f8174aace2798720f971c0645ecb0c25c248496d
Andrei Glingeanu <andrei.glingeanu@gmail.com>
2016-11-10 19:02:59 +0200

    Update manifest.php

--- manifest.php
+++ manifest.php
@@ -10,6 +10,10 @@ $manifest['description'] = __(
 	.' by adding optimized meta titles, keywords and descriptions.',
 	'fw'
 );
+$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-SEO-Extension';
+$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/seo/index.html#content';
+$manifest['author'] = 'ThemeFuse';
+$manifest['author_uri'] = 'http://themefuse.com/';
 $manifest['version'] = '1.0.8';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
